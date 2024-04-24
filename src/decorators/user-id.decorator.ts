0b686e69cd06ee1b1c53b458a751eac579ab7ade
Isac William <hailisac@hotmail.com>
2024-04-24 13:11:06 -0300

    Feat: Create decorator userId

---
+++ src/decorators/user-id.decorator.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext } from "@nestjs/common";
+import { createParamDecorator } from "@nestjs/common/decorators";
+import { authorizationToLoginPayload } from "src/utils/base-64-converter";
+
+export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
+    const { authorization } = ctx.switchToHttp().getRequest().headers;
+
+    const loginPayload = authorizationToLoginPayload(authorization);
+
+    console.log('authorization: ', authorization);
+
+    return loginPayload?.id;
+});
